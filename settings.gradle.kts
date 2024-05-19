76f66833bf8a6ea4ca0bf0ee8c13152d420a99fb
foselin <foselinalvarez@gmail.com>
2024-05-18 21:15:50 -0600

    cuarta actividad

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "almacenamiento_sa100821"
+include(":app")
